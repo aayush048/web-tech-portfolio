286cb4c5003085f520efff979cc3aa4f8809cb64
aayush048 <127837350+aayush048@users.noreply.github.com>
2023-05-23 23:00:08 +0545

    Update website.js

--- website.js
+++ website.js
@@ -1,19 +1,1 @@
-let sections= document.querySelectorAll('section');
-let navLinks= document.querySelectorAll('navbar ul li a');
 
-window.onscroll=() => {
-    sections.forEach(sec => {
-        let top= window.scrollY;
-        let offset = sec.offsetTop;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links =>{
-                links.classList.remove('active');
-                document.querySelectorAll('navbar ul li a')
-                [href*='+ id + ']').classlist.add('active');
-            });
-        };
-    });
-};
